8650216a5091325e4992dea51703201d50c31308
kareem172 <kareemkhalaf1722@gmail.com>
2025-04-14 15:18:57 +0200

    refactor(App): restructure App.vue to support dark mode and routing
    The changes include:
    - Replacing the static content with a dynamic layout that supports dark mode and routing.
    - Adding a NavBar component and integrating it with the Vuex store for dark mode state management.
    - Updating the styles to include CSS variables for theming and improving the overall layout structure.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,136 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div :class="isDarkMode && 'dark'">
+    <NavBar />
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapState } from "vuex";
+import NavBar from "./components/NavBar.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    NavBar,
+  },
+  computed: {
+    ...mapState(["isDarkMode"]),
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+:root {
+  --primary-color: hsl(211, 66%, 21%);
+  --primary-color-pressed: hsl(211, 66%, 18%);
+  --secondary-color: hsl(30, 20%, 79%);
+  --secondary-color-pressed: hsl(30, 20%, 60%);
+  --background-color: #f1efec;
+  --foreground-color: #030303;
+  --white-color: #fff;
+  --black-color: #000;
+  --font-size: 16px;
+  --font-family: sans-serif;
+  --border-radius: 0.25rem;
+  --border-radius-lg: 0.4rem;
+  --box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15);
+}
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+  transition: background-color 0.3s ease-in-out, color 0.3s ease-in-out;
+}
+.dark {
+  --secondary-color: hsl(211, 66%, 21%);
+  --secondary-color-pressed: hsl(211, 66%, 18%);
+  --primary-color-pressed: hsl(30, 20%, 79%);
+  --primary-color: hsl(30, 20%, 60%);
+  --background-color: hsl(211, 66%, 8%);
+  --foreground-color: #f1efec;
+}
+body {
+  min-height: 100dvh;
+  max-width: 100dvw;
+}
+#app,
+#app > div {
+  min-height: 100dvh;
+  max-width: 100dvw;
+  display: flex;
+  flex-direction: column;
+  background-color: var(--background-color);
+  color: var(--foreground-color);
+}
+
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0.75rem 2rem;
+  @media (max-width: 600px) {
+    padding: 0 0.5rem;
+  }
+}
+button,
+.btn {
+  background-color: var(--primary-color);
+  color: var(--white-color);
+  padding: 0.7rem 1.2rem;
+  border: none;
+  border-radius: var(--border-radius);
+  cursor: pointer;
+  font-size: 1rem;
+  transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
+  &:hover {
+    background-color: var(--primary-color-pressed);
+  }
+  &.outlined {
+    background-color: transparent;
+    color: var(--primary-color);
+    border: 1px solid var(--primary-color);
+    &:hover {
+      background-color: var(--primary-color-pressed);
+      color: var(--white-color);
+    }
+  }
+
+  &.icon {
+    background-color: transparent;
+    color: var(--primary-color);
+    padding: 0.75rem;
+    border-radius: 50%;
+    text-transform: uppercase;
+    cursor: pointer;
+    & * {
+      stroke-width: 0.15rem;
+    }
+    &:hover {
+      color: var(--primary-color-pressed);
+    }
+
+    .secondary {
+      background-color: var(--secondary-color);
+      color: var(--foreground-color);
+      &:hover {
+        background-color: var(--secondary-color-pressed);
+      }
+    }
+    .small {
+      padding: 0.25rem;
+      font-size: 1rem;
+      & * {
+        stroke-width: 0.1rem;
+      }
+    }
+  }
+}
+a {
+  text-decoration: none;
+}
+
+.flex-1 {
+  flex: 1;
 }
 </style>
