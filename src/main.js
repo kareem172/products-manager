dab187d855c87976cd73a5c04a737e1d9258ea8f
kareem172 <kareemkhalaf1722@gmail.com>
2025-04-14 15:18:39 +0200

    feat: add Vue Router and Vuex store to the application

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import store from "../store";
 
-createApp(App).mount('#app')
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: () => import("@/pages/Home.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+createApp(App).use(router).use(store).mount("#app");
