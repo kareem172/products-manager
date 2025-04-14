3d7d2161e176e2e059e6289666f0e35cfb295a36
kareem172 <kareemkhalaf1722@gmail.com>
2025-04-15 01:15:49 +0200

    feat(store): add product management functionality

--- store.js
+++ store.js
@@ -1,17 +1,63 @@
+import axios from "axios";
 import { createStore } from "vuex";
 
 const store = createStore({
   state: {
     isDarkMode: false,
+    products: [],
+    selectedProduct: null,
+    selectedProductId: null,
   },
   mutations: {
     toggleDarkMode(state) {
       state.isDarkMode = !state.isDarkMode;
     },
+    setAllProducts(state, payload) {
+      console.log("🚀 ~ setAllProducts ~ payload:", payload)
+      state.products = payload;
+    },
+    setProduct(state, payload) {
+      state.selectedProduct = payload;
+      state.selectedProductId = payload.id;
+    },
   },
   actions: {
     TOGGLE_DARK_MOOD({ commit }) {
       commit("toggleDarkMode");
+    },
+    async SET_ALL_PRODUCTS({ commit }) {
+      const res = await axios.get("http://localhost:3001/products");
+
+      commit("setAllProducts", res.data);
+    },
+    async SET_PRODUCT_BY_ID({ commit }, id) {
+      const res = await axios.get(`http://localhost:3001/products/${id}`);
+      commit("setProduct", res.data);
+    },
+    async ADD_PRODUCT({ commit }, newProduct) {
+      await axios.post("http://localhost:3001/products", {
+        ...newProduct,
+        rating: 4.5,
+        reviews: [],
+      });
+      const res = await axios.get("http://localhost:3001/products");
+      commit("setAllProducts", res.data);
+    },
+    async UPDATE_PRODUCT({ commit }, id, updatedProduct) {
+      await axios.patch(`http://localhost:3001/products/${id}`, {
+        ...updatedProduct,
+      });
+      const res = await axios.get("http://localhost:3001/products");
+      commit("setAllProducts", res.data);
+    },
+    async DELETE_PRODUCT({ commit }, id) {
+      await axios
+        .delete(`http://localhost:3001/products/${id}`)
+        .catch((err) => {
+          console.err(err);
+        });
+      const res = await axios.get("http://localhost:3001/products");
+      commit("setAllProducts", res.data);
     },
   },
 });
